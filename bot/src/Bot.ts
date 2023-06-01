aaab3c1ab4228a317dafa49b1deab6c763b851bd
brandon <brandonschiffer11@gmail.com>
2023-06-01 18:59:47 -0300

    intento typeorm

--- bot/src/Bot.ts
+++ bot/src/Bot.ts
@@ -1,11 +1,10 @@
 import { Client } from "discord.js";
 import interactionCreate from "./listeners/interactionCreate";
 import ready from "./listeners/ready";
-import {connect} from "./DBConnection"
+
 
 const token = require("../config.json").token;
 console.log("Bot is starting...");
-connect().catch(error => console.error(error));; //conexion a la base de datos
 const client = new Client({
     intents: []
 })
--- bot/src/DBConnection.ts
+++ bot/src/DBConnection.ts
@@ -1,17 +1,29 @@
-import { createConnection, Connection, getRepository } from 'typeorm';
-
-let connection: Connection | undefined;
-
-export async function connect() {
-  connection = await createConnection();
-}
+import "reflect-metadata"
+import { DataSource } from "typeorm"
 
 
-async function closeConnection() {
-  if (connection) {
-    await connection.close();
+const AppDataSource = new DataSource({
+    type: "mariadb",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "fiubito",
+    database: "fiubito_base_de_datos",
+    synchronize: true,
+    logging: false,
+    entities: ["src/entities/*.ts"],
   }
-}
+  )
+
+// to initialize initial connection with the database, register all entities
+// and "synchronize" database schema, call "initialize()" method of a newly created database
+// once in your application bootstrap
+
+AppDataSource.initialize()
+    .then(() => {
+        // here you can start to work with your database
+    })
+    .catch((error) => console.log(error))
 
 // async function doSomething() {
 //   if (!connection) {
