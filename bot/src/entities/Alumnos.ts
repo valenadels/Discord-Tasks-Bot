f666c4ae94d69d3c266cf20bdddebf006df70777
brandon <brandonschiffer11@gmail.com>
2023-06-01 01:17:38 -0300

    intento typeorm

---
+++ bot/src/entities/Alumnos.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+// @Entity()
+// export class Alumno {
+//   @PrimaryGeneratedColumn()
+//   id: number;
+
+//   @Column()
+//   nombre: string;
+
+//   @Column()
+//   apellido: string;
+
+//   @Column()
+//   padron: string;
+// }
