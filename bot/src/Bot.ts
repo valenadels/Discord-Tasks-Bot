df7ffca2b8d345954396d88eee13c403fb6fd80b
valenadels <v.adelsflugel@despegar.com>
2023-06-01 19:05:28 -0300

    database class

--- bot/src/Bot.ts
+++ bot/src/Bot.ts
@@ -1,10 +1,20 @@
 import { Client } from "discord.js";
 import interactionCreate from "./listeners/interactionCreate";
 import ready from "./listeners/ready";
+import { DatabaseConnection } from "./DBConnection";
 
 
 const token = require("../config.json").token;
 console.log("Bot is starting...");
+
+// Uso de la clase DatabaseConnection
+const dbConnection = new DatabaseConnection();
+dbConnection
+  .connect()
+  .then(() => {
+    // Aquí puedes comenzar a trabajar con la base de datos
+  })
+  .catch((error) => console.log(error));
 const client = new Client({
     intents: []
 })
--- bot/src/DBConnection.ts
+++ bot/src/DBConnection.ts
@@ -1,43 +1,24 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
 
+export class DatabaseConnection {
+  private dataSource: DataSource;
 
-const AppDataSource = new DataSource({
-    type: "mariadb",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "fiubito",
-    database: "fiubito_base_de_datos",
-    synchronize: true,
-    logging: false,
-    entities: ["src/entities/*.ts"],
+  constructor() {
+    this.dataSource = new DataSource({
+      type: "mariadb",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "fiubito",
+      database: "fiubito_base_de_datos",
+      synchronize: true,
+      logging: false,
+      entities: ["src/entities/*.ts"],
+    });
   }
-  )
 
-// to initialize initial connection with the database, register all entities
-// and "synchronize" database schema, call "initialize()" method of a newly created database
-// once in your application bootstrap
-
-AppDataSource.initialize()
-    .then(() => {
-        // here you can start to work with your database
-    })
-    .catch((error) => console.log(error))
-
-// async function doSomething() {
-//   if (!connection) {
-//     console.error('La conexión no ha sido establecida. Debes llamar a la función "connect" primero.');
-//     return;
-//   }
-
-//   const alumnosRepository = getRepository(Alumno);
-//   const carrerasRepository = getRepository(Carrera);
-//   const materiasRepository = getRepository(Materia);
-
-//   // Realiza las operaciones que desees utilizando los repositorios
-
-//   // Ejemplo: obtener todos los alumnos
-//   const alumnos = await alumnosRepository.find();
-//   console.log(alumnos);
-// }
+  connect(): Promise<DataSource> {
+    return this.dataSource.initialize();
+  }
+}
