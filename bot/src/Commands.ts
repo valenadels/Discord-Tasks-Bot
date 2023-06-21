a9d50ef01850adc253a004833bdc9d57698dae5f
brandonschiffer1 <brandonschiffer11@gmail.com>
2023-06-21 18:17:31 -0300

    agrego comandos

--- bot/src/Commands.ts
+++ bot/src/Commands.ts
@@ -1,13 +1,15 @@
 import { Command } from "./Command";
 import { Ayuda } from "./commands/Ayuda";
 import { Calendario } from "./commands/Calendario";
+import { Carrera } from "./commands/Carrera";
 
 import { Dollyfiuba } from "./commands/DollyFiuba";
 import { Fiubamap } from "./commands/FiubaMap";
 import { Fiubaplan } from "./commands/FiubaPlan";
 import { Hello } from "./commands/Hello";
 import { Login } from "./commands/LogIn";
+import { Materia } from "./commands/Materia";
 
 
 
-export const Commands: Command[] = [Hello, Calendario, Fiubamap, Dollyfiuba, Fiubaplan, Ayuda, Login]; 
+export const Commands: Command[] = [Hello, Calendario, Fiubamap, Dollyfiuba, Fiubaplan, Ayuda, Login, Carrera, Materia]; 
